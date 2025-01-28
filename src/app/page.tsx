b81a31c78daf688890267981af798435f12f8f0e
Antonio Nascimento <antonionascneto@gmail.com>
2025-01-28 10:41:14 -0300

    feat: insert images, finishing home page, create header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,55 @@
+import Header from "@/components/Header";
 import Image from "next/image";
+import Link from "next/link";
+import instagram from "../../public/icons8-instagram-64.png";
+import linkedin from "../../public/icons8-linkedin-50.png";
+import wpp from "../../public/icons8-whatsapp-64.png";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="h-screen flex flex-col items-center relative">
+      <Header />
+      {/* Hero section */}
+      <section className="w-full h-full flex flex-col justify-center pb-20 pl-32">
+        <h1 className="text-9xl uppercase font-light">Antonio</h1>
+        <h1 className="text-9xl uppercase font-bold">Nascimento</h1>
+        <h2 className="text-4xl tracking-[30px]">Web Developer</h2>
+        <div className="flex gap-5">
+          <Link
+            className="flex text-2xl justify-center items-center gap-2 border-[1px] w-fit py-2 px-8 rounded-full my-4 hover:bg-[#f1da8b] hover:text-zinc-600 transition-colors ease-in-out duration-500"
+            href={"/resume"}
+          >
+            Resume
+          </Link>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+          <Link
+            className="flex text-2xl justify-center items-center gap-2 border-[1px] w-fit py-2 px-8 rounded-full my-4 hover:bg-[#f1da8b] hover:text-zinc-600 transition-colors ease-in-out duration-500"
+            href={"/portfolio"}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Portfolio
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+      </section>
+      {/* Social Media */}
+      <section className="flex w-full justify-start pl-32 pb-10 gap-2 items-center">
+        <Link
+          href={"https://www.linkedin.com/in/antonio-nascimento-851579237/"}
           target="_blank"
-          rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          <Image src={linkedin} width={40} height={40} alt="link" />
+        </Link>
+        <Link target="_blank" href={"https://www.instagram.com/tonhohawk/"}>
+          <Image src={instagram} width={40} height={40} alt="link" />
+        </Link>
+        <Link
           target="_blank"
-          rel="noopener noreferrer"
+          href={
+            "https://api.whatsapp.com/send/?phone=5527993118171&text&type=phone_number&app_absent=0"
+          }
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <Image src={wpp} width={40} height={40} alt="link" />
+        </Link>
+      </section>
+    </main>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+
+export default function Header() {
+  const navList = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "About",
+      path: "/about",
+    },
+    {
+      name: "Resume",
+      path: "/resume",
+    },
+    {
+      name: "Portfolio",
+      path: "/portfolio",
+    },
+    {
+      name: "Skills",
+      path: "/skills",
+    },
+  ];
+  return (
+    <nav className="text-[rgb(243,231,219)] w-screen backdrop-filter backdrop-blur-md py-3">
+      <ul className="flex gap-10 w-full justify-center items-center">
+        {navList.map((item, index) => (
+          <li key={index}>
+            <Link
+              className="tracking-[3px] uppercase transition-all ease-in-out duration-300 hover:text-[#f1da8b]"
+              href={item.path}
+            >
+              {item.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
